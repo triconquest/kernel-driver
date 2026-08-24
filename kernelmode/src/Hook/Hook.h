76173cd87ecb6c4cf03cf8777458d7c51f402796
triconquest <juzzchg1092o0@gmail.com>
2026-08-24 15:24:21 +0400

    WIP: starting hooks

--- kernelmode/src/Hook/Hook.h
+++ kernelmode/src/Hook/Hook.h
@@ -1,10 +1,10 @@
-//#pragma once
-//#include "../Utils/Memory.h"
-//
-//// WIP: unfinished, needs a kernel function address
-//
-//namespace Hook {
-//	
-//	bool CallKernelFunction(void* kernelFunctionAddress);
-//	NTSTATUS HookHandler(PVOID calledParam);
-//}
+#pragma once
+#include "../Utils/Memory.h"
+
+// WIP: unfinished, needs a kernel function address
+
+namespace Hook {
+	
+	bool CallKernelFunction(void* kernelFunctionAddress);
+	NTSTATUS HookHandler(PVOID calledParam);
+}
